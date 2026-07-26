e74ad30411d86e2c578cb7822ea9e925080b2027
AliReZa174326 <alirreza1you3@gmail.com>
2026-07-26 14:26:40 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const text = "Welcome To My Website";
+
+let i = 0;
+
+function type(){
+
+if(i < text.length){
+
+document.getElementById("title").innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(type,100);
+
+}
+
+}
+
+type();
+
+function scrollProjects(){
+
+document.getElementById("projects").scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+}
+
+function toggleMenu(){
+
+    let menu = document.getElementById("nav-menu");
+
+    menu.classList.toggle("active");
+
+}
+
+document.querySelectorAll("#nav-menu a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+
+        document
+
+        .getElementById("nav-menu")
+
+        .classList.remove("active");
+
+    });
+
+});
+
+function openImage(src){
+
+let viewer=document.getElementById("image-viewer");
+let img=document.getElementById("big-image");
+
+img.src=src;
+viewer.style.display="flex";
+
+}
+
+
+function closeImage(){
+
+document.getElementById("image-viewer").style.display="none";
+
+}
